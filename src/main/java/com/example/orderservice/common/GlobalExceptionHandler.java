7a601e937caad2a2cabd60102567a33c8bf089ba
15221877741 <qingyangwang113@163.com>
2026-05-13 11:30:54 +0800

    123

--- src/main/java/com/example/orderservice/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/orderservice/common/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.example.orderservice.common;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Result<?> handleRuntimeException(RuntimeException e) {
         log.error("业务异常: {}", e.getMessage(), e);
         return Result.error(500, e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Result<?> handleException(Exception e) {
         log.error("系统异常: {}", e.getMessage(), e);
         return Result.error(500, "系统错误: " + e.getMessage());
